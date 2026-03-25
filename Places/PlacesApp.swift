85b418abfba8ffca71ba77a74ee9dfa75760fe77
Alexander van der Werff <avdwerff@gmail.com>
2026-03-25 16:49:30 +0100

    Initial setup
    - Swift 6
    - Default isolation @MainActor
    - Swift testing
    - MVVM

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Alexander van der Werff on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
